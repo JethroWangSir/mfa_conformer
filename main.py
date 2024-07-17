c2fe8efd812867350070bf1ff4bc1ea6726434e9
JethroWangSir <jethrowang0531@gmail.com>
2024-07-17 14:08:48 +0800

    20240717

--- main.py
+++ main.py
@@ -1,24 +1,77 @@
 from argparse import ArgumentParser
-from copy import deepcopy
 from typing import Any, Union
 import torch.distributed as dist
-from pytorch_lightning.plugins import DDPPlugin
-import random
-
+from pytorch_lightning.strategies import DDPStrategy
 import torch
 import torch.nn as nn
 import numpy as np
-
-from pytorch_lightning import LightningModule, Trainer, seed_everything
+from pytorch_lightning import LightningModule, Trainer, seed_everything, LightningDataModule
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
-from torch.nn import functional as F
+import pytorch_lightning as pl
 from torch.optim import AdamW
-from torch.optim.lr_scheduler import StepLR, CyclicLR
+from torch.optim.lr_scheduler import StepLR
+from torch.utils.data import DataLoader, Dataset
+import os
+import torchaudio
+import wandb
+from pytorch_lightning.loggers import WandbLogger
 
 from module.feature import Mel_Spectrogram
-from module.loader import SPK_datamodule
 import score as score
 from loss import softmax, amsoftmax
+
+class ChannelDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.classes = ['android', 'condenser', 'H8x', 'H8y', 'iOS', 'lavalier', 'PCmic']
+        self.class_to_idx = {cls_name: idx for idx, cls_name in enumerate(self.classes)}
+        self.samples = []
+        for cls in self.classes:
+            cls_dir = os.path.join(root_dir, cls)
+            for fname in os.listdir(cls_dir):
+                if fname.endswith('.wav'):
+                    self.samples.append((os.path.join(cls_dir, fname), self.class_to_idx[cls]))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        filepath, label = self.samples[idx]
+        waveform, sr = torchaudio.load(filepath)
+        if self.transform:
+            waveform = self.transform(waveform)
+        # print(f'Loaded {filepath} with label {label}')
+        return waveform, label
+
+def custom_collate_fn(batch):
+    max_length = max([waveform.size(1) for waveform, _ in batch])
+    padded_waveforms = []
+    labels = []
+    for waveform, label in batch:
+        if waveform.size(1) < max_length:
+            padding = max_length - waveform.size(1)
+            waveform = torch.nn.functional.pad(waveform, (0, padding))
+        padded_waveforms.append(waveform)
+        labels.append(label)
+    return torch.stack(padded_waveforms), torch.tensor(labels)
+
+class ChannelDataModule(LightningDataModule):
+    def __init__(self, data_dir, batch_size=32, num_workers=10):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def setup(self, stage=None):
+        self.train_dataset = ChannelDataset(os.path.join(self.data_dir, 'train'))
+        self.val_dataset = ChannelDataset(os.path.join(self.data_dir, 'test'))
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers, collate_fn=custom_collate_fn)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers, collate_fn=custom_collate_fn)
 
 class Task(LightningModule):
     def __init__(
@@ -28,14 +81,12 @@ class Task(LightningModule):
         batch_size: int = 32,
         num_workers: int = 10,
         max_epochs: int = 1000,
-        trial_path: str = "data/vox1_test.txt",
         **kwargs
     ):
         super().__init__()
         self.save_hyperparameters()
-        self.trials = np.loadtxt(self.hparams.trial_path, str)
         self.mel_trans = Mel_Spectrogram()
-
+        
         from module.resnet import resnet34, resnet18, resnet34_large
         from module.ecapa_tdnn import ecapa_tdnn, ecapa_tdnn_large
         from module.transformer_cat import transformer_cat
@@ -45,40 +96,36 @@ class Task(LightningModule):
 
         if self.hparams.encoder_name == "resnet18":
             self.encoder = resnet18(embedding_dim=self.hparams.embedding_dim)
-
+            print('Initialized resnet18')
         elif self.hparams.encoder_name == "resnet34":
             self.encoder = resnet34_large(embedding_dim=self.hparams.embedding_dim)
-
+            print('Initialized resnet34')
         elif self.hparams.encoder_name == "ecapa_tdnn":
             self.encoder = ecapa_tdnn(embedding_dim=self.hparams.embedding_dim)
-
+            print('Initialized ecapa_tdnn')
         elif self.hparams.encoder_name == "ecapa_tdnn_large":
             self.encoder = ecapa_tdnn_large(embedding_dim=self.hparams.embedding_dim)
-
+            print('Initialized ecapa_tdnn_large')
         elif self.hparams.encoder_name == "conformer":
-            print("num_blocks is {}".format(self.hparams.num_blocks))
             self.encoder = conformer(embedding_dim=self.hparams.embedding_dim, 
                     num_blocks=self.hparams.num_blocks, input_layer=self.hparams.input_layer)
-
+            print('Initialized conformer')
         elif self.hparams.encoder_name == "transformer_cat":
-            print("num_blocks is {}".format(self.hparams.num_blocks))
             self.encoder = transformer_cat(embedding_dim=self.hparams.embedding_dim, 
                     num_blocks=self.hparams.num_blocks, input_layer=self.hparams.input_layer)
-
+            print('Initialized transformer_cat')
         elif self.hparams.encoder_name == "conformer_cat":
-            print("num_blocks is {}".format(self.hparams.num_blocks))
             self.encoder = conformer_cat(embedding_dim=self.hparams.embedding_dim, 
                     num_blocks=self.hparams.num_blocks, input_layer=self.hparams.input_layer,
                     pos_enc_layer_type=self.hparams.pos_enc_layer_type)
-
+            print('Initialized conformer_cat')
         elif self.hparams.encoder_name == "conformer_weight":
-            print("num_blocks is {}".format(self.hparams.num_blocks))
             self.encoder = conformer_weight(embedding_dim=self.hparams.embedding_dim, 
                     num_blocks=self.hparams.num_blocks, input_layer=self.hparams.input_layer)
-
+            print('Initialized conformer_weight')
         else:
             raise ValueError("encoder name error")
-
+        
         if self.hparams.loss_name == "amsoftmax":
             self.loss_fun = amsoftmax(embedding_dim=self.hparams.embedding_dim, num_classes=self.hparams.num_classes)
         else:
@@ -91,69 +138,30 @@ class Task(LightningModule):
 
     def training_step(self, batch, batch_idx):
         waveform, label = batch
+        # print(f'waveform: {waveform}\t{waveform.shape}')
+        waveform = waveform.squeeze(1)
+        # print(f'squeezed_waveform: {waveform}\t{waveform.shape}')
+        # print(f'label: {label}\t{label.shape}')
         feature = self.mel_trans(waveform)
         embedding = self.encoder(feature)
         loss, acc = self.loss_fun(embedding, label)
         self.log('train_loss', loss, prog_bar=True)
-        self.log('acc', acc, prog_bar=True)
+        self.log('train_acc', acc, prog_bar=True)
         return loss
 
-    def on_test_epoch_start(self):
-        return self.on_validation_epoch_start()
-
-    def on_validation_epoch_start(self):
-        self.index_mapping = {}
-        self.eval_vectors = []
-
-    def test_step(self, batch, batch_idx):
-        self.validation_step(batch, batch_idx)
-
     def validation_step(self, batch, batch_idx):
-        x, path = batch
-        path = path[0]
+        waveform, label = batch
+        waveform = waveform.squeeze(1)
         with torch.no_grad():
-            x = self.mel_trans(x)
+            feature = self.mel_trans(waveform)
             self.encoder.eval()
-            x = self.encoder(x)
-        x = x.detach().cpu().numpy()[0]
-        self.eval_vectors.append(x)
-        self.index_mapping[path] = batch_idx
-
-    def test_epoch_end(self, outputs):
-        return self.validation_epoch_end(outputs)
-
-    def validation_epoch_end(self, outputs):
-        num_gpus = torch.cuda.device_count()
-        eval_vectors = [None for _ in range(num_gpus)]
-        dist.all_gather_object(eval_vectors, self.eval_vectors)
-        eval_vectors = np.vstack(eval_vectors)
-
-        table = [None for _ in range(num_gpus)]
-        dist.all_gather_object(table, self.index_mapping)
-
-        index_mapping = {}
-        for i in table:
-            index_mapping.update(i)
-
-        eval_vectors = eval_vectors - np.mean(eval_vectors, axis=0)
-        labels, scores = score.cosine_score(
-            self.trials, index_mapping, eval_vectors)
-        EER, threshold = score.compute_eer(labels, scores)
-
-        print("\ncosine EER: {:.2f}% with threshold {:.2f}".format(EER*100, threshold))
-        self.log("cosine_eer", EER*100)
-
-        minDCF, threshold = score.compute_minDCF(labels, scores, p_target=0.01)
-        print("cosine minDCF(10-2): {:.2f} with threshold {:.2f}".format(minDCF, threshold))
-        self.log("cosine_minDCF(10-2)", minDCF)
-
-        minDCF, threshold = score.compute_minDCF(labels, scores, p_target=0.001)
-        print("cosine minDCF(10-3): {:.2f} with threshold {:.2f}".format(minDCF, threshold))
-        self.log("cosine_minDCF(10-3)", minDCF)
-
+            embedding = self.encoder(feature)
+        loss, acc = self.loss_fun(embedding, label)
+        self.log('val_loss', loss, sync_dist=True, prog_bar=True)
+        self.log('val_acc', acc, sync_dist=True, prog_bar=True)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(
+        optimizer = AdamW(
             self.parameters(),
             self.hparams.learning_rate,
             weight_decay=self.hparams.weight_decay
@@ -161,31 +169,27 @@ class Task(LightningModule):
         scheduler = StepLR(optimizer, step_size=self.hparams.step_size, gamma=self.hparams.gamma)
         return [optimizer], [scheduler]
 
-    def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_idx,
-                       optimizer_closure, on_tpu, using_native_amp, using_lbfgs):
+    def optimizer_step(self, epoch, batch_idx, optimizer,
+                    optimizer_closure=None, on_tpu=None, using_native_amp=None, using_lbfgs=None):
         # warm up learning_rate
         if self.trainer.global_step < self.hparams.warmup_step:
-            lr_scale = min(1., float(self.trainer.global_step +
-                           1) / float(self.hparams.warmup_step))
-            for idx, pg in enumerate(optimizer.param_groups):
+            lr_scale = min(1., float(self.trainer.global_step + 1) / float(self.hparams.warmup_step))
+            for pg in optimizer.param_groups:
                 pg['lr'] = lr_scale * self.hparams.learning_rate
-        # update params
         optimizer.step(closure=optimizer_closure)
         optimizer.zero_grad()
 
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
-        (args, _) = parser.parse_known_args()
-
-        parser.add_argument("--num_workers", default=40, type=int)
+        parser.add_argument("--data_dir", type=str, default=None)
+        parser.add_argument("--num_workers", default=10, type=int)
         parser.add_argument("--embedding_dim", default=256, type=int)
-        parser.add_argument("--num_classes", type=int, default=1211)
+        parser.add_argument("--num_classes", type=int, default=7)
+        parser.add_argument("--max_epochs", type=int, default=30)
         parser.add_argument("--num_blocks", type=int, default=6)
-
         parser.add_argument("--input_layer", type=str, default="conv2d")
-        parser.add_argument("--pos_enc_layer_type", type=str, default="abs_pos")
-
+        parser.add_argument("--pos_enc_layer_type", type=str, default="rel_pos")
         parser.add_argument("--second", type=int, default=3)
         parser.add_argument('--step_size', type=int, default=1)
         parser.add_argument('--gamma', type=float, default=0.9)
@@ -193,65 +197,54 @@ class Task(LightningModule):
         parser.add_argument("--learning_rate", type=float, default=0.001)
         parser.add_argument("--warmup_step", type=float, default=2000)
         parser.add_argument("--weight_decay", type=float, default=0.000001)
-
         parser.add_argument("--save_dir", type=str, default=None)
         parser.add_argument("--checkpoint_path", type=str, default=None)
         parser.add_argument("--loss_name", type=str, default="amsoftmax")
         parser.add_argument("--encoder_name", type=str, default="resnet34")
-
         parser.add_argument("--train_csv_path", type=str, default="data/train.csv")
         parser.add_argument("--trial_path", type=str, default="data/vox1_test.txt")
         parser.add_argument("--score_save_path", type=str, default=None)
-
         parser.add_argument('--eval', action='store_true')
         parser.add_argument('--aug', action='store_true')
         return parser
 
-
 def cli_main():
     parser = ArgumentParser()
-    # trainer args
-    parser = Trainer.add_argparse_args(parser)
-
-    # model args
     parser = Task.add_model_specific_args(parser)
     args = parser.parse_args()
 
-    model = Task(**args.__dict__)
+    model = Task(**vars(args))
 
-    if args.checkpoint_path is not None:
-        state_dict = torch.load(args.checkpoint_path, map_location="cpu")["state_dict"]
-        model.load_state_dict(state_dict, strict=True)
-        print("load weight from {}".format(args.checkpoint_path))
-
-    assert args.save_dir is not None
-    checkpoint_callback = ModelCheckpoint(monitor='cosine_eer', save_top_k=100,
-           filename="{epoch}_{cosine_eer:.2f}", dirpath=args.save_dir)
+    dm = ChannelDataModule(data_dir=args.data_dir, 
+                           batch_size=args.batch_size, 
+                           num_workers=args.num_workers)
+    
+    checkpoint_callback = ModelCheckpoint(
+        monitor='val_acc',
+        save_top_k=3,
+        mode='max',
+        filename='{epoch:02d}-{val_acc:.2f}',
+        dirpath=args.save_dir
+    )
     lr_monitor = LearningRateMonitor(logging_interval='step')
+    wandb_logger = WandbLogger(project='MFA-Conformer')
 
-    # init default datamodule
-    print("data augmentation {}".format(args.aug))
-    dm = SPK_datamodule(train_csv_path=args.train_csv_path, trial_path=args.trial_path, second=args.second,
-            aug=args.aug, batch_size=args.batch_size, num_workers=args.num_workers, pairs=False)
-    AVAIL_GPUS = torch.cuda.device_count()
     trainer = Trainer(
-            max_epochs=args.max_epochs,
-            plugins=DDPPlugin(find_unused_parameters=False),
-            gpus=AVAIL_GPUS,
-            num_sanity_val_steps=-1,
-            sync_batchnorm=True,
-            callbacks=[checkpoint_callback, lr_monitor],
-            default_root_dir=args.save_dir,
-            reload_dataloaders_every_n_epochs=1,
-            accumulate_grad_batches=1,
-            log_every_n_steps=25,
-            )
-    if args.eval:
-        trainer.test(model, datamodule=dm)
-    else:
-        trainer.fit(model, datamodule=dm)
+        max_epochs=args.max_epochs,
+        strategy='ddp',
+        devices=1,
+        accelerator='gpu',
+        sync_batchnorm=True,
+        callbacks=[checkpoint_callback, lr_monitor],
+        logger=wandb_logger,
+        default_root_dir=args.save_dir,
+        reload_dataloaders_every_n_epochs=1,
+        accumulate_grad_batches=1,
+        log_every_n_steps=25
+    )
+    
+    trainer.fit(model, datamodule=dm)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    wandb.login()
     cli_main()
-
--- start.sh
+++ start.sh
@@ -1,44 +1,34 @@
-encoder_name="conformer_cat" # conformer_cat | ecapa_tdnn_large | resnet34
-embedding_dim=192
-loss_name="amsoftmax"
-
-dataset="vox"
-num_classes=7205
-num_blocks=6
-train_csv_path="data/train.csv"
-
-input_layer=conv2d2
-pos_enc_layer_type=rel_pos # no_pos| rel_pos 
-save_dir=experiment/${input_layer}/${encoder_name}_${num_blocks}_${embedding_dim}_${loss_name}
-trial_path=data/vox1_test.txt
-
-mkdir -p $save_dir
-cp start.sh $save_dir
-cp main.py $save_dir
-cp -r module $save_dir
-cp -r wenet $save_dir
-cp -r scripts $save_dir
-cp -r loss $save_dir
-echo save_dir: $save_dir
+#!/bin/bash
 
 export CUDA_VISIBLE_DEVICES=0
-python3 main.py \
-        --batch_size 200 \
-        --num_workers 40 \
-        --max_epochs 30 \
-        --embedding_dim $embedding_dim \
-        --save_dir $save_dir \
-        --encoder_name $encoder_name \
-        --train_csv_path $train_csv_path \
-        --learning_rate 0.001 \
-        --encoder_name ${encoder_name} \
-        --num_classes $num_classes \
-        --trial_path $trial_path \
-        --loss_name $loss_name \
-        --num_blocks $num_blocks \
-        --step_size 4 \
-        --gamma 0.5 \
-        --weight_decay 0.0000001 \
-        --input_layer $input_layer \
-        --pos_enc_layer_type $pos_enc_layer_type 
 
+DATA_DIR='/share/nas169/jethrowang/CADA-GAN/hat_asr_aligned/channel_encoder'
+BATCH_SIZE=2
+NUM_WORKERS=16
+MAX_EPOCHS=30
+ENCODER_NAME='conformer_cat'
+LOSS_NAME='amsoftmax'
+LEARNING_RATE=0.001
+WARMUP_STEP=327999
+WEIGHT_DECAY=0.0000001
+STEP_SIZE=4
+GAMMA=0.5
+SAVE_DIR='./results'
+INPUT_LAYER=conv2d2
+CHECKPOINT_PATH='./channel_encoder'
+
+python main.py \
+  --data_dir $DATA_DIR \
+  --batch_size $BATCH_SIZE \
+  --num_workers $NUM_WORKERS \
+  --max_epochs $MAX_EPOCHS \
+  --encoder_name $ENCODER_NAME \
+  --loss_name $LOSS_NAME \
+  --learning_rate $LEARNING_RATE \
+  --warmup_step $WARMUP_STEP \
+  --weight_decay $WEIGHT_DECAY \
+  --step_size $STEP_SIZE \
+  --gamma $GAMMA \
+  --save_dir $SAVE_DIR \
+  --input_layer $INPUT_LAYER \
+  --checkpoint_path $CHECKPOINT_PATH
